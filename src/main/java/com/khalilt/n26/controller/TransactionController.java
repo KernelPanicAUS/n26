eee1f0e6b7fd358c705695ed6e7f7287b6786dea
Thomas Khalil <thomas@khalilt.com>
2016-09-25 15:23:21 +0200

    Add app base with static responses

---
+++ src/main/java/com/khalilt/n26/controller/TransactionController.java
@@ -0,0 +1,51 @@
+package com.khalilt.n26.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+@RestController
+@EnableAutoConfiguration
+@RequestMapping(value = "/transactionservice", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class TransactionController {
+
+    @RequestMapping(value = "/transaction", method = RequestMethod.GET)
+    @ResponseBody
+    public String index() {
+        return "{\"status\": "+ HttpStatus.OK.value() +"}";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/transaction/{txnId}", method = RequestMethod.PUT)
+    public String createTxn(@PathVariable Long txnId, @RequestBody Map<String, Object> payload) {
+        return "{\"status\": "+ HttpStatus.OK.value() +", \"body\": " + txnId + "}";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/transaction/{txnId}", method = RequestMethod.GET)
+    public String fetchTxn(@PathVariable Long txnId) {
+        return "{\"amount\": 1000, \"type\": car, \"parent_id\": " + txnId + "}";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/types/{type}", method = RequestMethod.GET)
+    public String fetchTxnIdsByType(@PathVariable String type) {
+        System.out.print(type);
+        return "[10001,10002,10003]";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/sum/{txnId}", method = RequestMethod.GET)
+    public String fetchTxnSumForTxn(@PathVariable Long txnId) {
+        System.out.println(txnId);
+        return "{\"sum\": 10023.00}";
+    }
+}
---
+++ src/test/java/com/khalilt/n26/controller/TransactionControllerTest.java
@@ -0,0 +1,35 @@
+package com.khalilt.n26.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionControllerTest {
+
+
+
+    @Test
+    public void testIndex() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateTxn() throws Exception {
+
+    }
+
+    @Test
+    public void testFetchTxn() throws Exception {
+
+    }
+
+    @Test
+    public void testFetchTxnIdsByType() throws Exception {
+
+    }
+
+    @Test
+    public void testFetchTxnSumForTxn() throws Exception {
+
+    }
+}
